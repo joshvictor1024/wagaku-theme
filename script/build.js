950fb1c3b54b8096224851863d5b99beb41418e1
joshvictor1024 <joshvictoronfire@gmail.com>
2021-03-07 11:51:26 +0800

    Fix package README; Fix package.json semVer

--- script/build.js
+++ script/build.js
@@ -1,4 +1,6 @@
-const execSync = require("child_process").execSync;
+const { execSync } = require("child_process");
+const { copyFileSync } = require("fs");
 
 execSync("node script/build/theme.js");
 execSync("uglifyjs src/extension.js -m -o dist/extension.js");
+copyFileSync('README.md', 'dist/README.md');
--- script/clean.js
+++ script/clean.js
@@ -2,3 +2,4 @@ const { unlinkSync } = require("fs");
 
 unlinkSync("dist/themes/wagaku-midnight-color-theme.json");
 unlinkSync("dist/extension.js");
+unlinkSync("dist/README.md");
